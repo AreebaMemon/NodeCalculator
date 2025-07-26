ceb88093add6207c050584ae5b79b42c63273d98
Areeba Memon <103986750+AreebaMemon@users.noreply.github.com>
2025-07-26 15:54:16 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(bodyparser.urlencoded({ extended: false}));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", (req, res) => {
